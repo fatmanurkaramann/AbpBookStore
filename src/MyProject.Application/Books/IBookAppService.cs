24b2d4b19039c6b7334287d3c908099402fd18ec
Fatma Nur Karaman <fatmanur.karaman@outlook.com>
2023-09-20 13:15:43 +0300

    IBookAppservice added

---
+++ src/MyProject.Application/Books/IBookAppService.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Application.Services;
+using MyProject.Books.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Books
+{
+    public interface IBookAppService : IAsyncCrudAppService<BookDto,int, PagedBookResultRequestDto, CreateBookDto, BookDto>
+    {
+    }
+}
